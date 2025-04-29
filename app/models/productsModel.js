3cb7160e9e44626c89f059299da874c68453fa48
Hernan <hernansaa88@gmail.com>
2025-04-29 17:03:44 +0200

    update product schema

--- app/models/productsModel.js
+++ app/models/productsModel.js
@@ -1,14 +1,16 @@
 const mongoose = require('mongoose');
 
 const productSchema = new mongoose.Schema({
-  name: { type: String, required: true },         // e.g. JetSki, Cuatriciclo, etc.
-  category: { type: String, required: true },     // e.g. "vehicle", "equipment", "surfboard"
+  name: { type: String, required: true },                     // e.g. JetSki, Cuatriciclo, etc.
+  category: { type: String, required: true },                 // e.g. "vehicle", "equipment", "surfboard"
   requiresHelmet: { type: Boolean, default: false },
   requiresLifeJacket: { type: Boolean, default: false },
-  capacity: { type: Number, default: 1 },         // e.g. 1 or 2 persons
-  durationPerSlot: { type: Number, default: 30 }, // Minutes
-  maxDurationConsecutive: { type: Number, default: 90 },     // Minutes
-  isForKids: { type: Boolean, default: false }    // For surfboards
+  capacity: { type: Number, default: 1 },                     // e.g. 1 or 2 persons
+  durationPerSlot: { type: Number, default: 30 },             // Minutes
+  maxDurationConsecutive: { type: Number, default: 90 },      // Minutes
+  minReserveAnticipation: { type: Number, default: 2 },       // Hours
+  maxReserveAnticipation: { type: Number, default: 48 },      // Hours
+  isForKids: { type: Boolean, default: false }                // For surfboards
 });
 
 const Product = mongoose.model('Product', productSchema);
