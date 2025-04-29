77d3085c589f693a8b26a5aa752c71869d03863b
Hernan <hernansaa88@gmail.com>
2025-04-29 13:01:21 +0200

    serve index html file

--- app/app.js
+++ app/app.js
@@ -5,11 +5,13 @@
 
 // Import core modules
 const express = require("express");
+// const path = require('path');
 const http = require("http");
+const bodyParser = require('body-parser');
 const cors = require("cors");
 
 // Import app routes
-const routes = require("./routes/routes");
+const routes = require("./routes");
 
 // Create an Express app
 const app = express();
@@ -17,9 +19,15 @@ const app = express();
 // Set up middleware
 app.use(cors());            // Enable Cross-Origin Resource Sharing
 app.use(express.json());    // Parse JSON request bodies
+app.use(bodyParser.urlencoded({extended: false}))
 
 // Use imported routes
 app.use("/", routes);       // Mount all routes at the root path
+
+// // Views
+// app.get('/', (req, res) => {
+//     res.sendFile(path.join(__dirname,'views', 'index.html'));
+// })
 
 // Choose port from environment or default to 5000
 const PORT = process.env.PORT || 5000;
@@ -28,3 +36,6 @@ const PORT = process.env.PORT || 5000;
 http.createServer(app).listen(PORT, () => {
     console.log(`🚀 Server is running at http://localhost:${PORT}`);
 });
+
+
+module.exports = app
